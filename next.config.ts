eba9125d196a379a9678b58ab93fe62ce9d437cb
bajoao <bartolomeugasparbg@gmail.com>
2025-10-17 21:38:05 +0100

    Add build and static manifests for Next.js application
    - Created a new image cache file in the Next.js cache directory.
    - Added a build manifest to track the application's pages and their associated chunks.
    - Introduced a client middleware manifest, currently empty.
    - Added an SSG (Static Site Generation) manifest to manage static pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'blog.fotto.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.artguru.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'photokit.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
